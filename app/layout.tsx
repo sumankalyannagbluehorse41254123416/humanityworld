ee2ddbd543e4788b911ed717c924c6dd8b0ae0da
Dev_Srijani <srijani.bluehorse@mail.com>
2025-11-28 13:15:25 +0530

    hgfd

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
+    </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -2,7 +2,8 @@ export default function WhatsAppButton() {
   return (
     <div className="whatsapp">
       <a href="https://wa.me/9967868731" target="_blank" rel="noopener noreferrer">
-        <i className="ri-whatsapp-fill" style={{ color: "#25D366" }}></i>
+        {/* <i className="ri-whatsapp-fill" style={{ color: "#25D366" }}></i> */}
+        <i className="fa-brands fa-whatsapp" style={{ color: "#25D366" }}></i>
       </a>
     </div>
   );
