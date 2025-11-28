94ff09cd5d1506a92ca68575b3c88bea52b91988
santana <santanarana3@gmail.com>
2025-11-28 16:53:25 +0530

    changes

---
+++ app/our-approach/page.tsx
@@ -0,0 +1,15 @@
+import HeroBanner from "@/components/our-approach/HeroBanner";
+import ApproachSection from "@/components/our-approach/ApproachSection";
+import ProgramsSection from "@/components/our-approach/ProgramsSection";
+import EmpathySection from "@/components/our-approach/EmpathySection";
+
+export default function OurApproach() {
+    return (
+        <>
+        <HeroBanner/>
+        <ApproachSection/>
+        <ProgramsSection/>
+        <EmpathySection/>
+        </>
+    );
+}
