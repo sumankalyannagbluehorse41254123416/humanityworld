e2ddfe17a990087922f1392a5d5e26852878147f
santana <santanarana3@gmail.com>
2025-12-04 16:57:43 +0530

    changes

---
+++ app/donate/page.tsx
@@ -0,0 +1,6 @@
+import DonateSection from "@/components/donate/DonateSection";
+export default function Donate() {
+    return (
+        <DonateSection />
+    )
+}
---
+++ app/razorpay/page.tsx
@@ -0,0 +1,6 @@
+import PaymentDetails from "@/components/razorpay/PaymentDetails";
+export default function Razorpay() {
+    return (
+        <PaymentDetails />
+    )
+}
